f149708de3bcfc6d62f54289891951245949fd25
zahra <zahrajoulaei@gmail.com>
2017-10-18 00:37:38 +0330

    route added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -12,6 +13,7 @@ import { HeroService }         from './hero.service';
     BrowserModule,
     FormsModule
   ],
+
   declarations: [
     AppComponent,
     HeroDetailComponent,
@@ -22,5 +24,13 @@ import { HeroService }         from './hero.service';
   ],
   bootstrap: [ AppComponent ]
 })
+
 export class AppModule {
 }
+
+RouterModule.forRoot([
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+])
